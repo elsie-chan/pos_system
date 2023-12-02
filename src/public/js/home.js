f77e9d20864bd84c28466f19649a154763f65365
Bùi Thái Ngọc <ngocbangbangpa@gmail.com>
2023-12-03 02:54:18 +0700

    Fix UI

--- src/public/js/home.js
+++ src/public/js/home.js
@@ -1,16 +1,21 @@
+const sidebarItems = document.querySelectorAll('.slidebar-item');
+const homeSection = document.getElementById('home-section');
+const paymentSection = document.getElementById('payment-section');
+const paymentSuccessSection = document.getElementById('payment-success-section');
+const accountSection = document.getElementById('account-section')
+const orHomeSection = document.getElementById('order-home');
+const orPaymentSection = document.getElementById('order-payment');
+const orPaymentSuccessSection = document.getElementById('order-payment-success');
+const btnPay = document.getElementById('btnPay');
+
 $(document).ready(function () {
   uploadItemSlidebar();
+  btnPay.addEventListener("click", function() {
+    showPaymentSuccessful();
+  })
 });
 
 function uploadItemSlidebar() {
-    const sidebarItems = document.querySelectorAll('.slidebar-item');
-    const homeSection = document.getElementById('home-section');
-    const paymentSection = document.getElementById('payment-section');
-    const paymentSuccessSection = document.getElementById('payment-success-section');
-    const orHomeSection = document.getElementById('order-home');
-    const orPaymentSection = document.getElementById('order-payment');
-    const orPaymentSuccessSection = document.getElementById('order-payment-success');
-
     sidebarItems.forEach(function (item) {
       item.addEventListener('click', function () {
       // Xoá class 'item_active' từ tất cả các item
@@ -30,32 +35,44 @@ function uploadItemSlidebar() {
       }
     });
   });
+}
 
-  function showHome() {
-    homeSection.style.display = 'block';
-    paymentSection.style.display = 'none';
-    paymentSuccessSection.style.display = 'none';
-    orHomeSection.style.display = 'block';
-    orPaymentSection.style.display = 'none';
-    orPaymentSuccessSection.style.display = 'none';
-  }
+function showHome() {
+  homeSection.style.display = 'block';
+  paymentSection.style.display = 'none';
+  paymentSuccessSection.style.display = 'none';
+  orHomeSection.style.display = 'block';
+  orPaymentSection.style.display = 'none';
+  orPaymentSuccessSection.style.display = 'none';
+  accountSection.style.display = 'none';
+}
 
-  function showPayment() {
-    homeSection.style.display ='none';
-    paymentSection.style.display = 'block';
-    paymentSuccessSection.style.display = 'none';
-    orHomeSection.style.display = 'none';
-    orPaymentSection.style.display = 'block';
-    orPaymentSuccessSection.style.display = 'none';
-  }
+function showPayment() {
+  homeSection.style.display ='none';
+  paymentSection.style.display = 'block';
+  paymentSuccessSection.style.display = 'none';
+  orHomeSection.style.display = 'none';
+  orPaymentSection.style.display = 'block';
+  orPaymentSuccessSection.style.display = 'none';
+  accountSection.style.display = 'none';
+}
 
-  const showAccount = () => {
-    homeSection.style.display ='none';
-    paymentSection.style.display = 'none';
-    paymentSuccessSection.style.display = 'block';
-    orHomeSection.style.display = 'none';
-    orPaymentSection.style.display = 'none';
-    orPaymentSuccessSection.style.display = 'block';
-  }
+const showAccount = () => {
+  homeSection.style.display ='none';
+  paymentSection.style.display = 'none';
+  paymentSuccessSection.style.display = 'none';
+  orHomeSection.style.display = 'none';
+  orPaymentSection.style.display = 'none';
+  orPaymentSuccessSection.style.display = 'none';
+  accountSection.style.display = 'block';
+}
 
+const showPaymentSuccessful = () => {
+  homeSection.style.display ='none';
+  paymentSection.style.display = 'none';
+  paymentSuccessSection.style.display = 'block';
+  orHomeSection.style.display = 'none';
+  orPaymentSection.style.display = 'none';
+  orPaymentSuccessSection.style.display = 'block';
+  accountSection.style.display = 'none';
 }
