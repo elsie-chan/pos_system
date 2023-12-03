a05e01b7b9fc3e57fc9c970f486392c5aa995d00
Bùi Thái Ngọc <ngocbangbangpa@gmail.com>
2023-12-03 21:18:57 +0700

    fix error 500

--- src/routes/home.route.js
+++ src/routes/home.route.js
@@ -8,16 +8,12 @@ import ApiProductController from "../controllers/api/api.product.controller.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.render('layouts/home', {title: 'Home'})
-});
-
 router.get("/add_product_to_session", AuthMiddleware.requireRole([Roles.ADMIN, Roles.STAFF]), validation, SellController.addProductToSession.bind(SellController));
 
-router.get("/product", async ( req, res ) => {
+router.get("/", async ( req, res ) => {
     let products = await ApiProductController.getAll(req, res);
-    // console.log(products)
-    res.render('layouts/home', {title: 'Product', products: products.data, pagination: products.pagination});
+    //console.log(products)
+    res.render('layouts/home', {title: 'Home', products: products.data, pagination: products.pagination});
 })
 
 export default router;
